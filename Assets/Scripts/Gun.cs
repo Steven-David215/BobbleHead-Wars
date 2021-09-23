32e6784c9499d7d63fe0efbb4ad328833985f7cd
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-09-23 18:12:28 -0400

    Added projectile and created prefab. Created Gun script and the fireBullet function.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+
+    void fireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
