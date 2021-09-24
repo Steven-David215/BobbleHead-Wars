ec74cea46808adafe3dad6a7b6bbd674fabc4234
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-09-24 16:18:03 -0400

    Added hinge to marine to make him a bobble head and adjusted the code and layer collisions to make it happen, mid chapter 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody head;
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
     // Start is called before the first frame update
@@ -18,5 +19,20 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+    
+    //Called consistently compared to update.
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+            0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            //todo
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
