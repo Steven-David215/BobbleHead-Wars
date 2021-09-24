507c9d4d7860e05bfbdc2ffc9d3393ada2f8c805
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-09-24 15:38:32 -0400

    added character controller to the marine and modified the playercontroller to reflect the changes. Added box colider to column prefab.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime; //time.deltatime is important and makes things work.
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.position = pos; //updates the gameobject which could be represented using 'this.' but isn't necessary.
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+            0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
