67ff72f0e03d76e9af2827617cbae0c0eb427b8a
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-09-24 16:32:07 -0400

    End of chapter 4, implemented Raycasting to turn character following mouse pointer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody head;
     public float moveSpeed = 50.0f;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
@@ -33,6 +35,23 @@ public class PlayerController : MonoBehaviour
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+        if (Physics.Raycast(ray, out hit, 1000, layerMask,
+            QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+            Vector3 targetPosition = new Vector3(hit.point.x,
+                transform.position.y, hit.point.z);
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+                transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                rotation, Time.deltaTime * 10.0f);
         }
     }
 }
