3fb84290bf8b664e0101b04420938f3c74f615c2
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-10-05 22:22:27 -0400

    Adding Power Up chapter 7.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,20 +6,49 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
 
+    private float currentTime;
     private AudioSource audioSource;
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
 
     public void fireBullet()
     {
 
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
 
-        bullet.transform.position = launchPosition.position;
+        if(isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity =
+                (transform.right + transform.forward / 0.5f) * 100;
 
-        bullet.GetComponent<Rigidbody>().velocity = 
-        transform.parent.forward * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity =
+                ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 
     // Start is called before the first frame update
