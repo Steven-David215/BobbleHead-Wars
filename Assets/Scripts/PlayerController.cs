c9f140e634fadd836e7c51d6237325a4b568a235
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-10-05 21:21:14 -0400

    Finished Chapter 6 -Marine walks.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
+    public Animator bodyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,10 +31,12 @@ public class PlayerController : MonoBehaviour
             0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            //todo
+            bodyAnimator.SetBool("IsMoving", false);
+            
         }
         else
         {
+            bodyAnimator.SetBool("IsMoving", true);
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
         RaycastHit hit;
