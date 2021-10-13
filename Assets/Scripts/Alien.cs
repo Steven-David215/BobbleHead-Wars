c51238f0fd1836d307031c075deb17d3f2c56498
Steven 'SteveDave' David <stevedave215@gmail.com>
2021-10-13 08:52:35 -0400

    Chapter 8 - Decapitating the enemy. Lo and behold their heads fall off then behave awkwardly, will show in class if it also doesn't resolve before hand.

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -9,10 +9,12 @@ public class Alien : MonoBehaviour
     public Transform target;
     public float navigationUpdate;
     public UnityEvent OnDestroy;
+    public Rigidbody head;
+    public bool isAlive = true;
     private float navigationTime = 0;
     private NavMeshAgent agent;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -21,25 +23,43 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
+
     }
+
     void OnTriggerEnter(Collider other)
     {
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
-        Die();
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
+
     public void Die()
     {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 6.0f, 3.0f);
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        head.GetComponent<SelfDestruct>().Initiate();
         Destroy(gameObject);
     }
 }
